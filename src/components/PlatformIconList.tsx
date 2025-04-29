3969528a5bf6d559eced2062ad97c37b4d8e4ed7
Adonay Amar <adonayamar@gmail.com>
2025-04-29 00:52:18 -0400

    Sort the games (fixed platform errors)

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -9,7 +9,7 @@ import {
 import { MdPhoneIphone } from "react-icons/md";
 import { BsNintendoSwitch } from "react-icons/bs";
 import { BsGlobe } from "react-icons/bs";
-import { Platform } from "../hooks/useGames";
+import { Platform } from "../hooks/usePlatforms";
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
