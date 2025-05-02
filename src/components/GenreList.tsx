3da9a7f7b7ce38282ff9270296e9dca36a50a32a
Adonay Amar <adonayamar@gmail.com>
2025-05-01 23:55:47 -0400

    Implemented useQuery in useGenre and GenreList

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -34,7 +34,7 @@ export const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
         Genres
       </Heading>
       <List>
-        {data.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
