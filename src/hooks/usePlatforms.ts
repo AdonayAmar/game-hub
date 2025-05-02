abfb9f541a502b154b0edf7d8f2ae82932863d6f
Adonay Amar <adonayamar@gmail.com>
2025-05-02 00:30:17 -0400

    Implemented useQuery in usePlatform and PlatformSelector

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
 import platforms from "../data/platforms";
+import { FetchResponse } from "./useData";
 
 export interface Platform {
   id: number;
@@ -6,9 +9,15 @@ export interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => ({
-  data: platforms,
-  error: null,
-});
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
