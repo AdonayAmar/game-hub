225ac5947f224e10eab549ee5b7e336b9a021363
Adonay Amar <adonayamar@gmail.com>
2025-05-06 00:52:02 -0400

    Fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
@@ -12,19 +13,21 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justify={"space-between"} marginBottom={3}>
-          <PlatformIconList
-            platform={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize="2xl">
-          {game.name}
-          <Emoji rating={game.rating_top} />
-        </Heading>
-      </CardBody>
+      <Link to={"/games/" + game.slug}>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justify={"space-between"} marginBottom={3}>
+            <PlatformIconList
+              platform={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">
+            {game.name}
+            <Emoji rating={game.rating_top} />
+          </Heading>
+        </CardBody>
+      </Link>
     </Card>
   );
 };
